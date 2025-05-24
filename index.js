e216198f6e0c751819f087de8a0d2e698005fcf1
Chance Sly <chancesly06@gmail.com>
2025-05-24 18:10:18 -0400

    Made divs for left middle right aligned center

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const counter=document.getElementById("Counter");
+const Cookiebtn=document.getElementById("Cookiebtn");
+let count=0;
+Cookiebtn.addEventListener("click",function(){
+    count=count+1
+    console.log(count) 
+    counter.textContent=count;
+})
