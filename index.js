cbaa60e4dcdd9638ad907591b2ad01d634541b34
Chance Sly <chancesly06@gmail.com>
2025-05-25 13:33:17 -0400

    added cookie img and first market option

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const counter=document.getElementById("Counter");
 const Cookiebtn=document.getElementById("Cookiebtn");
+const stickPriceEl=document.getElementById("stickPriceEl")
+const stickBtn=document.getElementById("stickBtn")
 let count=0;
+let stickPrice=15;
 Cookiebtn.addEventListener("click",function(){
     count=count+1
     console.log(count) 
     counter.textContent=count;
+})
+stickPriceEl.textContent=16;
+stickBtn.addEventListener("click",function(){
+    stickPrice=Math.floor(stickPrice*1.2)
+    stickPriceEl.textContent=stickPrice
 })
