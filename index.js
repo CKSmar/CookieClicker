a9bd92189d26831848a172f3919c562b7b592d20
Chance Sly <chancesly06@gmail.com>
2025-05-25 22:15:36 -0400

    made the stick button work and give cookies

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const stickPriceEl=document.getElementById("stickPriceEl")
 const stickBtn=document.getElementById("stickBtn")
 let count=0;
 let stickPrice=15;
+let numSticks=0
 Cookiebtn.addEventListener("click",function(){
     count=count+1
     console.log(count) 
-    counter.textContent=count;
+    counter.textContent=Math.floor(count);
 })
-stickPriceEl.textContent=16;
 stickBtn.addEventListener("click",function(){
-    stickPrice=Math.floor(stickPrice*1.2)
-    stickPriceEl.textContent=stickPrice
+    if(count>=stickPrice){
+        count=count-stickPrice
+        counter.textContent=count;
+        numSticks+=1
+        stickPrice=Math.floor(stickPrice*1.15)
+        stickPriceEl.textContent=stickPrice
+        
+    }
+    
 })
+setTimeout(function() {
+    setInterval(function(){
+        count+=numSticks/30;
+        counter.textContent = Math.floor(count);
+    },100)
+})
+//If you change your name to chance is the goat, you get access to admin comands and can give your self unlimited cookies
